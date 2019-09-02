4d42b2c22540db7e1b192ff2610678697be33db3
bing-deng <804433988@qq.com>
2019-09-02 13:52:53 +0900

    Update websocket.py

--- python/websocket.py
+++ python/websocket.py
@@ -1,1 +1,33 @@
 * https://websockets.readthedocs.io/en/stable/intro.html#basic-example
+
+  
+  
+  * 服务器接收
+  ```
+  var ws = new WebSocket("ws://127.0.0.1:5678/");
+
+
+setInterval(function(){
+    var rsi =$('span.pane-legend-item-value.pane-legend-line')[12]
+    console.log(rsi.textContent)
+    ws.send(rsi.textContent)
+}, 3000);
+
+
+  ```
+  
+  
+  * 浏览器发送
+  
+  ```
+  var ws = new WebSocket("ws://127.0.0.1:5678/");
+
+
+setInterval(function(){
+    var rsi =$('span.pane-legend-item-value.pane-legend-line')[12]
+    console.log(rsi.textContent)
+    ws.send(rsi.textContent)
+}, 3000);
+
+
+  ```
